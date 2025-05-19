dd2b6a8f16ea39996de3b2ba1655831465afec55
AxelcichelloArciel <acichello@arciel.com>
2025-05-19 17:39:10 -0300

    seguir cargando para hacer el test

--- src/main/java/com/empresa/excusas/Main.java
+++ src/main/java/com/empresa/excusas/Main.java
@@ -4,6 +4,9 @@ import com.empresa.excusas.interfaces.ModoOperacion;
 import com.empresa.excusas.model.clases.EmpleadoExcusador;
 import com.empresa.excusas.model.clases.Excusa;
 import com.empresa.excusas.model.clases.encargados.Recepcionista;
+import com.empresa.excusas.model.clases.excusas.ExcusaCompleja;
+import com.empresa.excusas.model.clases.excusas.ExcusaInverosimil;
+import com.empresa.excusas.model.clases.excusas.ExcusaModerada;
 import com.empresa.excusas.model.clases.excusas.ExcusaTrivial;
 import com.empresa.excusas.model.clases.modoOperacion.ModoNormal;
 import com.empresa.excusas.model.clasesAbstractas.Empleado;
@@ -15,9 +18,17 @@ public class Main {
     public static void main(String[] args) {
 
         TipoExcusa trivial = new ExcusaTrivial();
+        TipoExcusa inverosimil = new ExcusaInverosimil();
+        TipoExcusa compleja = new ExcusaCompleja();
+        TipoExcusa moderada = new ExcusaModerada();
+
+
         EmpleadoExcusador emp = new EmpleadoExcusador("Axel Cichello", "csaxel49@gmail.com", 40143488);
 
         Excusa excusa1 = new Excusa("Me quede dormido", trivial, emp);
+        Excusa excusa2 = new Excusa("Me quede dormido", inverosimil, emp);
+        Excusa excusa3 = new Excusa("Me quede dormido", compleja, emp);
+        Excusa excusa4 = new Excusa("Me quede dormido", moderada, emp);
 
         ModoOperacion modoNormal = new ModoNormal();
 
